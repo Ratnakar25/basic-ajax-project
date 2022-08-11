31a827b1f880262991786c9a43a05d4862cdb595
Ratnakar25 <ratnakar0579.be20@chitkara.edu.in>
2022-08-11 18:38:00 +0530

    Third Commit (added Blogs form,footer, and working on xml)

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
-var xhr = new XMLHttpRequest();
-xhr.open("GET", "https://reqres.in/api/products/3", true);
-xhr.onload = function() {
-    console.log(xhr.responseText);
+const xhr = new XMLHttpRequest();
+const source =
+    "https://jsonplaceholder.typicode.com/users?utm_source=Mailerlite&utm_medium=E-mail&utm_campaign=Test%20Series&utm_term=2022-08-09";
+xhr.open("GET", source, true);
+xhr.onreadystatechange = () => {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+        const response = JSON.parse(xhr.responseText);
+        console.log(response);
+        let output = "";
+        for (let i = 0; i < response.length; i++) {
+            output += `
+        <div class="container">
+            <div class="team">
+                <h2 class="name"> - ${response[i].name}</h2>
+                <p class="title">${response[i].username}</p>
+                <p class="body">${response[i].email}</p>
+            </div>
+        </div>`;
+        }
+        document.querySelector(".container").innerHTML = output;
+    }
 };
 xhr.send();
-
-fetch('https://jsonplaceholder.typicode.com/users?utm_source=Mailerlite&utm_medium=E-mail&utm_campaign=Test%20Series&utm_term=2022-08-09')
-    .then(response => response.json())
-    .then(json => console.log(json))
